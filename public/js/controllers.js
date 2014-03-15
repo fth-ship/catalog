0d287581646bb224e3cf025bfdd2e850d5d32be3
Felquis Gimenes <frgformenton@gmail.com>
2014-03-15 20:05:38 -0300

    Add search for movies with Angular

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,5 +1,31 @@
-function indexHandler() {}
+function indexHandler($scope, $http) {
+  $scope.q = '';
+  $scope.movies = [];
+
+  function fetchData () {
+    function successHandler (data) {
+      $scope.movies = data.result;
+    }
+
+    function errorHandler (data) {
+      console.log('Error loading movies!');
+    }
+
+    $http
+      .get('/api/movies?q=' + $scope.q)
+      .success(successHandler)
+      .error(errorHandler);
+  }
+
+  function onKeyPress (event) {
+    if (event.keyCode === 13) {
+      fetchData();
+    }
+  }
+
+  $scope.onKeyPress = onKeyPress;
+}
 
 angular
   .module('catalog.controllers', [])
-  .controller('index', [indexHandler]);
+  .controller('index', ['$scope', '$http', indexHandler]);
